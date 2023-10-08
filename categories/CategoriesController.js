71c665078772ac47ef20d72745007b766f350e89
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-10-08 12:38:44 -0300

    categories form

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,10 +1,6 @@
 import express from "express";
 export const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("Categories route");
-});
-
 router.get("/admin/categories/new", (req, res) => {
-  res.send("Route for new categories creation");
+  res.render("admin/categories/new");
 });
