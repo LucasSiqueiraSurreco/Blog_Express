02718cccc97be7441fefa06b0708bd571cbcd432
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-10-08 16:13:28 -0300

    categories form to db

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,6 +1,21 @@
 import express from "express";
+import { Category } from "./Category.js";
+import slugify from "slugify";
+
 export const router = express.Router();
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+  let { title } = req.body;
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title),
+    }).then(() => res.redirect("/"));
+  } else {
+    res.redirect("/admin/categories/new");
+  }
 });
