d95d612b452b987e67dc81e5970955852c923b18
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-10-09 16:05:28 -0300

    feat: edit

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -16,13 +16,31 @@ router.get("/admin/categories", (req, res) => {
   });
 });
 
+router.get("/admin/categories/edit/:id", (req, res) => {
+  let { id } = req.params;
+
+  if (isNaN(id)) {
+    res.redirect("/admin/categories");
+  }
+
+  Category.findByPk(id)
+    .then((categories) => {
+      if (categories != undefined) {
+        res.render("admin/categories/edit", { categories: categories });
+      } else {
+        res.redirect("/admin/categories");
+      }
+    })
+    .catch((err) => res.redirect("/admin/categories"));
+});
+
 router.post("/categories/save", (req, res) => {
   let { title } = req.body;
   if (title != undefined) {
     Category.create({
       title: title,
       slug: slugify(title),
-    }).then(() => res.redirect("/"));
+    }).then(() => res.redirect("/admin/categories"));
   } else {
     res.redirect("/admin/categories/new");
   }
