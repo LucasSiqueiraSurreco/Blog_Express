8412a29e4513d6a51256ff376c99f58aa830c547
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-10-10 16:47:31 -0300

    article select

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,10 +1,26 @@
 import express from "express";
 export const router = express.Router();
+import { Category } from "../categories/Category.js";
+import { Article } from "./Article.js";
+import slugify from "slugify";
 
 router.get("/articles", (req, res) => {
   res.send("Articles route");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
+});
+
+router.post("/articles/save", (req, res) => {
+  let { title, body, category } = req.body;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category,
+  });
 });
